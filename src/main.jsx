580b8852cd7b82e2c2a208ac5a9bb7b9cfd0e17c
Ethan1416 <ethangrucza@gmail.com>
2026-05-19 10:31:44 -0700

    Configure for GitHub Pages: hash routing + /homehub/ base

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import PhoneView from './views/PhoneView.jsx'
 import TvView from './views/TvView.jsx'
 import PinGate from './components/PinGate.jsx'
@@ -8,12 +8,12 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PinGate><PhoneView /></PinGate>} />
         <Route path="/tv" element={<TvView />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,21 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/homehub/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg'],
       manifest: {
+        scope: '/homehub/',
+        start_url: '/homehub/',
         name: 'HomeHub',
         short_name: 'HomeHub',
         description: 'Shared calendar + Claude status for the household',
         theme_color: '#0f1117',
         background_color: '#0f1117',
         display: 'standalone',
-        start_url: '/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
